c8d9b17352097b6e32b7ec1809d0f4c4402f83b7
AMRHall <48449028+AMRHall@users.noreply.github.com>
2019-03-11 17:33:36 +0000

    Add files via upload

---
+++ Python/NMRshuttle_VBlist_v4.py
@@ -0,0 +1,6 @@
+ # Import libraries and define functions
+import PyTrinamic
+from PyTrinamic.connections.serial_tmcl_interface import serial_tmcl_interface
+from PyTrinamic.modules.TMCM_1640 import TMCM_1640
+import time
+import datetime 
