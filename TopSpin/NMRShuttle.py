e783cfb6561a28e2589ae59bac9fa19b2fb163a5
AMRHall <48449028+AMRHall@users.noreply.github.com>
2019-03-25 15:01:47 +0000

    Include option for 1D and 2D spectra

--- TopSpin/NMRShuttle.py
+++ TopSpin/NMRShuttle.py
@@ -1,7 +1,7 @@
 
 #
 # NMRshuttle.py
-# Version 2.0, Mar 2019
+# Version 2.1, Mar 2019
 #
 # Andrew Hall 2019 (c)
 # a.m.r.hall@soton.ac.uk
@@ -9,7 +9,7 @@
 # Python script for controlling NMR low field shuttle with Trinamic TMCM-1060 or TMCM-1160 motor using TopSpin.
 # Includes options for different NMR tubes and for arbitarily defined velocity/distance profiles.
 # Requires setup file containing motor parameters. Also requires PyTrinamic and Serial libraries.
-# 
+# Requires Python 3.6 or later.
 #
 
 
@@ -21,6 +21,9 @@ from PyTrinamic.connections.serial_tmcl_interface import serial_tmcl_interface
 from PyTrinamic.modules.TMCM_1160 import TMCM_1160
 import time
 import datetime 
+
+# Print information about script
+print("\n\nNMRshuttle_VBlist\nVersion 5\nThis program is for controlling a NMR low field shuttle using a TMCM-1060 or TMCM-1160 motor.\nCopyright (c) Andrew Hall, 2019\nFor further details see https://github.com/AMRHall/NMR_Shuttle/blob/master/README.md\n\n\n")
 
 # Import default values from setup file
 setup = NMRShuttleSetup.NMRShuttle()
@@ -81,6 +84,9 @@ module.setUserVariable(9,0)
 # Get number of transients for each field strength
 NS = int(sys.argv[4])
 
+# Get number of 2D slices
+TD = int(sys.argv[5])
+
 # Fetch desired magnetic field strength (mT)
 BSample = int(sys.argv[3])
 print(str("\nMagnetic field strength = " + str(BSample) +  " mT"))
@@ -104,35 +110,41 @@ print(str("Number of steps = " + str(steps)))
 module.setUserVariable(1,steps)
   
 # Check position of sample and wait until finished
-n = 0
 up = 'n'
 start_position = module.actualPosition()
-while n < NS:	
-	# Check for errors in motor
-	error = module.userVariable(9)
-	if error == 1:		#Shutdown error from light gate
-		print("\nEmergency stop detected. Aborting acquisition.")
-		errflag += 1
-		break
-	elif error == 2:       #Stall detected
-		print("\nStall detected. Aborting acquisition.")
-		errflag += 1
-		break
+m = 0
+while m < TD:
+	n = 0
+	while n < NS:	
+		# Check for errors in motor
+		error = module.userVariable(9)
+		if error == 1:		#Shutdown error from light gate
+			print("\nEmergency stop detected. Aborting acquisition.")
+			errflag += 1
+			break
+		elif error == 2:       #Stall detected
+			print("\nStall detected. Aborting acquisition.")
+			errflag += 1
+			break
 	
-	#Check sample position
-	position = module.userVariable(8)
-	if up == 'n' and position == 1:
-		up = 'y'
-		print("Sample up.")
-	if up == 'y' and position == 0:
-		up = 'n'
-		n += 1
-		print("Sample down.")
-		print(str("Completed transient " + str(n) + " of " + str(NS) + " at magnetic field strength of " + str(BSample) + " mT"))
-	if position == 2 and mode == 2:
-		curr_position = ((module.actualPosition()-start_position)*-Circ)/NStep
-		speed = int(eval(ramp))
-		module.setTargetSpeed(speed)
+		#Check sample position
+		position = module.userVariable(8)
+		if up == 'n' and position == 1:
+			up = 'y'
+			print("Sample up.")
+		if up == 'y' and position == 0:
+			up = 'n'
+			n += 1
+			print("Sample down.")
+			if TD == 1:
+				print(str("Completed scan " + str(n) + " of " + str(NS) + " at magnetic field strength of " + str(BSample) + " mT\n"))
+			else:
+				print(str("Completed scan " + str(n) + "/" + str(NS) + " for 2D slice" + str(m) + "/" + str(TD) + " at magnetic field strength of " + str(BSample) + " mT\n"))
+		if position == 2 and mode == 2:
+			curr_position = ((module.actualPosition()-start_position)*-Circ)/NStep
+			speed = int(eval(ramp))
+			module.setTargetSpeed(speed)
+	td += 1
 		
 # Once sequence is complete for all magnetic field strengths:
 # Set motor distance back to zero for safety
