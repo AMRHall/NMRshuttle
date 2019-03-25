440224222d8a59d38e5f7cbb6071dee55722e290
AMRHall <48449028+AMRHall@users.noreply.github.com>
2019-03-25 15:02:15 +0000

    Include options for 1D and 2D spectra

--- TopSpin/runNMRShuttle.py
+++ TopSpin/runNMRShuttle.py
@@ -1,15 +1,19 @@
 import os
 
-GETCURDATA
+os.chdir("/opt/topspin3.5pl7/exp/stan/nmr/py/user")
 
 mode = GETPAR("CNST 11")
 type = GETPAR("CNST 12")
 BSample = GETPAR("CNST 20")
 ns = GETPAR("NS")
+dim = GETPAR("PARMODE")
+if dim != 0:
+  td = GETPAR("TD",1)
+else:
+  td = 1
 
-command = str("python3.6 user/NMRShuttle.py " + mode + " " + type + " " + BSample + " " + ns)
+command = str("python3.6 NMRShuttle.py " + mode + " " + type + " " + BSample + " " + ns + " " + td)
 
 print(command)
 os.system(command)
 
-ZG
