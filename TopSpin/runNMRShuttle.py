ac40788a8ef4e796a0cefba713f2933a565408bb
AMRHall <48449028+AMRHall@users.noreply.github.com>
2019-04-02 18:03:15 +0100

    More error checking routines

--- TopSpin/runNMRShuttle.py
+++ TopSpin/runNMRShuttle.py
@@ -35,14 +35,30 @@ if type != 1 or mode != 2 or mode !=3:
   EXIT()
 
 if (distance < 0)  or (distance > setup.maxHeight):
-	ERRMSG("Field strength out of range!",modal=1)
+  ERRMSG("Field strength out of range! (CNST20)",modal=1)
   ct = XCMD("STOP")
   EXIT()
+	
   
 #If mode = constant time, calculate the speed that the motor needs to run at.
 if mode == 3:
   speed = 0.5 * (accel * setTime - math.sqrt(accel) * math.sqrt(-4 * distance + accel * setTime**2))
+  if 4 * distance > accel * (setTime**2):
+    ERRMSG("Speed out of range! (CNST30)\nIncrease the sample motion time (D10).",modal=1)
+    ct = XCMD("STOP")
+    EXIT()
+
+if (accel < 0)  or (accel > 465.434):
+  ERRMSG("Acceleration out of range! (CNST31)",modal=1)
+  ct = XCMD("STOP")
+  EXIT()
+	
+if (speed < 0)  or (accel > 30.5027):
+  ERRMSG("Speed out of range! (CNST30)\nIf using constant time mode, consider increasing the sample motion time (D10).",modal=1)
+  ct = XCMD("STOP")
+  EXIT()
   
+	
 #Call NMRShuttle.py with arguments  
 command = str("python3.6 NMRShuttle.py " + mode + " " + type + " " + BSample + " " + ns + " " + td + " " + speed + " " + accel)
 
