7c45e83299259dc1dcbbb5f39eb5023c5135e572
AMRHall <48449028+AMRHall@users.noreply.github.com>
2019-04-04 16:50:47 +0100

    Fix bugs

--- TopSpin/runNMRShuttle.py
+++ TopSpin/runNMRShuttle.py
@@ -8,8 +8,8 @@ setup = NMRShuttleSetup.NMRShuttle()
 os.chdir("/opt/topspin3.5pl7/exp/stan/nmr/py/user")
 
 #Get parameters from TopSpin
-mode = int(GETPAR("CNST 11"))
-type = int(GETPAR("CNST 12"))
+mode = GETPAR("CNST 11")
+type = GETPAR("CNST 12")
 BSample = GETPAR("CNST 20")
 ns = GETPAR("NS")
 dim = GETPAR("PARMODE")
@@ -24,26 +24,36 @@ else:
   
 distance = float(setup.b*(((setup.B0/float(BSample))-1)**(1/setup.a)))
 
+print("\n\n")
 #For mode 3 calculate motor speed.
-if mode == (1 or 2):
-  time = setTime
+if int(mode) == 1:
+	print("Constant velocity mode")
+elif int(mode) == 2:
+	print("Velocity sweep mode") 
 #If mode = constant time, calculate the speed that the motor needs to run at.
-elif mode == 3:
-  speed = 0.5 * (accel * setTime - math.sqrt(accel) * math.sqrt(-4 * distance + accel * setTime**2))
-  if 4 * distance > accel * (setTime**2):
-    ERRMSG("Speed out of range! (CNST30)\nIncrease the sample motion time (D10).",modal=1)
-    ct = XCMD("STOP")
-    EXIT()
+elif int(mode) == 3:
+	print("Constant time mode") 
+	speed = 0.5 * (accel * setTime - math.sqrt(accel) * math.sqrt(-4 * distance + accel * setTime**2))
+	if 4 * distance > accel * (setTime**2):
+		ERRMSG("Speed out of range! (CNST30)\nIncrease the sample motion time (D10).",modal=1)
+		ct = XCMD("STOP")
+		EXIT()
 else:
   ERRMSG("Invalid value for operation mode (CNST11).",modal=1)
   ct = XCMD("STOP")
   EXIT()
 
 #Error messages
-if type != (1 or 2 or 3):
-  ERRMSG("Invalid value for tube type (CNST12).",modal=1)
-  ct = XCMD("STOP")
-  EXIT()
+if int(type) == 1:
+	print("Standard 5mm tube")
+elif int(type) == 2:
+	print("5mm high pressure tube")
+elif int(type) == 3:
+	print("10mm high pressure tube")
+else:
+	ERRMSG("Invalid value for tube type (CNST12).",modal=1)
+	ct = XCMD("STOP")
+	EXIT()
 
 if (distance < 0)  or (distance > setup.maxHeight):
   ERRMSG("Field strength out of range! (CNST20)",modal=1)
@@ -62,8 +72,9 @@ if (speed < 0)  or (accel > 30.5027):
   
 
 #Call NMRShuttle.py with arguments  
-command = str("python3.6 NMRShuttle.py " + str(mode) + " " + str(type) + " " + BSample + " " + ns + " " + td + " " + str(speed) + " " + str(accel) + " " + ramp)
+command = str("python3.6 NMRShuttle.py " + mode + " " + type + " " + BSample + " " + ns + " " + td + " " + str(speed) + " " + str(accel) + " " + ramp)
 
 print(command)
 os.system(command)
 
+#ct = XCMD("ZG")
