2153f91f171e23942418ab6fc64569a96479b1c6
AMRHall <48449028+AMRHall@users.noreply.github.com>
2019-04-09 17:30:32 +0100

    Update runNMRShuttle.py

--- TopSpin/runNMRShuttle.py
+++ TopSpin/runNMRShuttle.py
@@ -1,80 +1,128 @@
+
+#
+# runNMRShuttle.py
+# Version 1.2, Apr 2019
+#
+# Andrew Hall 2019 (c)
+# a.m.r.hall@soton.ac.uk
+#
+# Python script for calling NMR low field shuttle operation and starting acquisition in Topspin.
+# Includes options for different NMR tubes and for arbitarily defined velocity/distance profiles.
+# Requires setup file containing motor parameters.
+#
+
+
+#Import libraries
 import os
 import math
 import NMRShuttleSetup
+import subprocess
 
 setup = NMRShuttleSetup.NMRShuttle()
 
 #Set directory to TopSpin python user library
 os.chdir("/opt/topspin3.5pl7/exp/stan/nmr/py/user")
 
-#Get parameters from TopSpin
-mode = GETPAR("CNST 11")
-type = GETPAR("CNST 12")
-BSample = GETPAR("CNST 20")
-ns = GETPAR("NS")
-dim = GETPAR("PARMODE")
-speed = float(GETPAR("CNST 30"))
-accel = float(GETPAR("CNST 31"))
-setTime = GETPAR("D10")
-ramp = str(GETPAR("USERA 1"))
-if dim != 0:
-  td = GETPAR("TD",1)
-else:
-  td = 1
-  
-distance = float(setup.b*(((setup.B0/float(BSample))-1)**(1/setup.a)))
+#Get parameters from TopSpin and NMRShuttleSetup.py
+mode = int(GETPAR("CNST 11"))
+tubeType = int(GETPAR("CNST 12"))
+BSample = float(GETPAR("CNST 20"))
+ns = int(GETPAR("NS"))
+dim = int(GETPAR("PARMODE"))
+motionTime = float(GETPAR("D 10"))
 
-print("\n\n")
-#For mode 3 calculate motor speed.
-if int(mode) == 1:
-	print("Constant velocity mode")
-elif int(mode) == 2:
-	print("Velocity sweep mode") 
-#If mode = constant time, calculate the speed that the motor needs to run at.
-elif int(mode) == 3:
-	print("Constant time mode") 
-	speed = 0.5 * (accel * setTime - math.sqrt(accel) * math.sqrt(-4 * distance + accel * setTime**2))
-	if 4 * distance > accel * (setTime**2):
-		ERRMSG("Speed out of range! (CNST30)\nIncrease the sample motion time (D10).",modal=1)
+if str(GETPAR("USERA 1")) != "None":
+  ramp = str(GETPAR("USERA 1"))
+else:
+  ramp = setup.ramp
+
+if GETPAR("CNST 30") != "1":
+  speed = float(GETPAR("CNST 30"))
+else:
+  speed = setup.speed
+
+if GETPAR("CNST 31") != "1":
+  accel = float(GETPAR("CNST 31"))
+else:
+  accel = setup.accel
+if (accel < 0)  or (accel > 465.434):
+  ERRMSG("Acceleration out of range! (CNST31)", modal=1, title="NMR Shuttle Error")
+  ct = XCMD("STOP")
+  EXIT()
+
+if dim == 0:
+  td = "1"
+else:
+  td = GETPAR("TD",1)
+
+
+#Calculate distance that motor needs to move to achieve the desired field strength.
+distance = float(setup.b*(((setup.B0/BSample)-1)**(1/setup.a)))
+if (distance < 0)  or (distance > setup.maxHeight):
+  ERRMSG("Field strength out of range! (CNST20)", modal=1, title="NMR Shuttle Error")
+  ct = XCMD("STOP")
+  EXIT()
+
+
+#For modes 1 and 3, estimate the amount of time needed to complete sample motion. For constant time mode, calculate the speed that the motor needs to run at.
+if mode == 1:
+	print("\n\nConstant velocity mode")
+	if (accel * (speed/accel)**2) >= distance:
+		motionTime = 2 * math.sqrt(distance/accel)
+	else:
+		motionTime = 2 * (speed/accel) + (distance - accel*((speed/accel)**2))/speed
+	PUTPAR("D 10",motionTime)
+	
+elif mode == 2:
+	print("\n\nVelocity sweep mode")
+	currField = 0
+	motionTime = 0
+	while currField <= BSample:
+		dspeed = float(eval(ramp)) - dspeed
+		ddistance = float(setup.b*(((setup.B0/(0.01*BSample))-1)**(1/setup.a)))
+		motionTime += ddistance/dspeed
+		currField += (0.01*BSample)
+	
+elif mode == 3:	
+	print("\n\nConstant time mode") 
+	if 4 * distance > accel * (motionTime**2):
+		ERRMSG("Speed out of range! (CNST30)\nIncrease the sample motion time (D10).", modal=1, title="NMR Shuttle Error")
 		ct = XCMD("STOP")
 		EXIT()
+	else:
+		speed = 0.5 * (accel * motionTime - math.sqrt(accel) * math.sqrt(-4 * distance + accel * motionTime**2))
 else:
-  ERRMSG("Invalid value for operation mode (CNST11).",modal=1)
+  ERRMSG("Invalid value for operation mode (CNST11).", modal=1, title="NMR Shuttle Error")
   ct = XCMD("STOP")
   EXIT()
+
 
 #Error messages
-if int(type) == 1:
+if tubeType == 1:
 	print("Standard 5mm tube")
-elif int(type) == 2:
+elif tubeType == 2:
 	print("5mm high pressure tube")
-elif int(type) == 3:
+elif tubeType == 3:
 	print("10mm high pressure tube")
 else:
-	ERRMSG("Invalid value for tube type (CNST12).",modal=1)
+	ERRMSG("Invalid value for tube type (CNST12).", modal=1, title="NMR Shuttle Error")
 	ct = XCMD("STOP")
 	EXIT()
-
-if (distance < 0)  or (distance > setup.maxHeight):
-  ERRMSG("Field strength out of range! (CNST20)",modal=1)
-  ct = XCMD("STOP")
-  EXIT()
-
-if (accel < 0)  or (accel > 465.434):
-  ERRMSG("Acceleration out of range! (CNST31)",modal=1)
-  ct = XCMD("STOP")
-  EXIT()
 	
-if (speed < 0)  or (accel > 30.5027):
-  ERRMSG("Speed out of range! (CNST30)\nIf using constant time mode, consider increasing the sample motion time (D10).",modal=1)
+if (speed < 0)  or (speed > setup.maxSpeed):
+  ERRMSG("Speed out of range! (CNST30)\nIf using constant time mode, consider increasing the sample motion time (D10).", modal=1, title="NMR Shuttle Error")
   ct = XCMD("STOP")
   EXIT()
   
 
+	
 #Call NMRShuttle.py with arguments  
-command = str("python3.6 NMRShuttle.py " + mode + " " + type + " " + BSample + " " + ns + " " + td + " " + str(speed) + " " + str(accel) + " " + ramp)
+command = "python3.6 NMRShuttle.py "
+arguments = str(mode) + " " + str(tubeType) + " " + str(BSample) + " " + str(ns) + " " + td + " " + str(speed) + " " + str(accel) + " " + ramp
 
-print(command)
-os.system(command)
+print(command + arguments)
+subprocess.Popen(command + arguments, shell=True)
 
-#ct = XCMD("ZG")
+
+#Start acquisition
+ct = XCMD("ZG")
