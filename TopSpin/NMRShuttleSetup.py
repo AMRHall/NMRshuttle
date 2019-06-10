c783c74723583161d420feb1e262bcd2c8a5f4c2
AMRHall <48449028+AMRHall@users.noreply.github.com>
2019-06-10 22:02:13 +0100

    Update calculation parameters

--- TopSpin/NMRShuttleSetup.py
+++ TopSpin/NMRShuttleSetup.py
@@ -7,6 +7,9 @@ class NMRShuttle(object):
 
   # Serial port name for motor
   serial =            "/dev/ttyACM0"
+  
+  # Motor model
+  model =             "TMCM-1160"
 
   # Maximum allowed height (cm)
   maxHeight =         90
@@ -14,7 +17,7 @@ class NMRShuttle(object):
   # Magnetic field strength at magnet core (mT)
   B0 =                7056.39197
 
-  # Fitting parameters for NMR field map [equation of the form BSample = B0/(1+(x/b)^a)]
+  # Fitting parameters for NMR field map [equation of the form B(z) = B0/(1+(z/b)^a)]
   a =                 5.75
   b =                 20.34
 
@@ -26,17 +29,17 @@ class NMRShuttle(object):
 
 
 
-  # Target motor speed (cm/s) (0...30.5027)
+  # Target motor speed (cm/s) (0...30.518 with default settings)
   speed =             25
 
-  # Maximum motor acceleration (cm/s^2) (0...465.4). This can be adapted by changing motor rampDiv setting
+  # Maximum motor acceleration (cm/s^2) (0...465.5 with default settings). This can be adapted by changing motor rampDiv setting or motor wheel diameter
   accel =             22.74
 
-  # Maximum motor speed (cm/s) (0...30.5027). This can be adapted by changing motor pulDiv setting
-  maxSpeed =          30.5027
+  # Maximum motor speed (cm/s) (0...30.518 with default settings). This can be adapted by changing motor pulDiv setting or motor wheel diameter
+  maxSpeed =          30.518
 
-  # Velocity ramp equation (as a function of field strength)
-  ramp =              "1-0.9/(1+math.exp(-0.001*(currField-5000)))"
+  # Velocity ramp equation. Equation must be expressed as a Python math equation in terms of sample position (z) or local field strength (Bz)
+  ramp =              "1-0.9/(1+math.exp(-0.001*(Bz-5000)))"
 
   
   
@@ -48,7 +51,18 @@ class NMRShuttle(object):
   # Motor pulse division (0...13). DO NOT CHANGE THIS NUMBER UNLESS YOU HAVE READ THE MANUAL! 
   # Changes maximum speed of motor. Decreasing the number increases speed, but compromises precision.
   pulDiv =          3
+  
+  # Motor ramp division (0...13). DO NOT CHANGE THIS NUMBER UNLESS YOU HAVE READ THE MANUAL! 
+  # The exponent of the scaling factor for the ramp generator.
+  rampDiv =         7
+  
+ 
 
+
+# *****************************
+# STALL GUARD SETTINGS ARE USED TO LIMIT THE TORQUE THAT THE MOTOR CAN APPLY.
+# SEE THE MANUAL FOR MORE DETAILS.
+# *****************************
 
 class stallGuard_stan(object):
   # Stall guard settings for standard glass tube
