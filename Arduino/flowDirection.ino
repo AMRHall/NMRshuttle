80cd9a5ce6cb000e19ee3fa4f8d3da4b96141ee6
AMRHall <48449028+AMRHall@users.noreply.github.com>
2020-07-09 09:39:10 +0100

    Error diagnosis and manual control functions

--- Arduino/flowDirection.ino
+++ Arduino/flowDirection.ino
@@ -8,19 +8,20 @@ Relays automatically switch direction after set delay. TTL trigger
 from spectrometer blocks the relay for 10 seconds to prevent the 
 flow direction switching during acquisition.
 
-Delay for switching can be sent to arduino using the command 'D XX'
-where XX is the delay time in seconds. Sending the command '?' returns
-the current status of the valves; 0= de-energised, 1= energised.
+Delay for switching can be sent to arduino as interger values in 
+seconds. Sending the command 'STATUS' returns the current status 
+(ON, OFF, HOLD, ERROR). The command 'POS' returns the postion of 
+the valves (1= ON, 0= OFF). The valve position can be set manually
+using the commands 'SET_VALVE_ON' and 'SET_VALVE_OFF'. The module
+is activated and deactivated using the commands 'SWITCH_ON' and 
+'SWITCH_OFF'.
 
 (c) Andrew Hall 2020
 a.m.r.hall@ed.ac.uk
 
-version 1.0
+version 1.1
 Jul 2020
 */
-
-
-bool debug = false;
 
 
 // define pins used by relay shield
@@ -32,10 +33,13 @@ const int ttl = A0;
 
 
 // Variables that will change:
+bool ttl_working = true;          // status of ttl input line
+bool on = true;                   // module on/off state
 int valveState = LOW;             // valveState used to set the valve
 int switchDelay = 60;             // default value for valve switching delay
 bool hold = false;                // hold state used to prevent switching during acquisition
 int holdDelay = 10;               // delay to prevent switching during acquisition
+String errs;                      // string for holding error messages
 
 
 // Generally, you should use "unsigned long" for variables that hold time
@@ -54,6 +58,13 @@ void setup() {
   pinMode(relay3, OUTPUT);
   pinMode(relay4, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
+  
+  // TTL input should be high when module switched on (active low logic)
+  // if no signal is detected return error message.
+  if (analogRead(ttl) < 500) {
+    ttl_working = false;
+    errs += "ERROR: No signal from TTL input";
+  }
 }
 
 
@@ -65,67 +76,109 @@ void loop() {
     
     String line = Serial.readStringUntil('\n');
     
-    // check to see if status request from computer
-    if (line.indexOf("?") >= 0) {
-      Serial.print("STATUS: ");
+    // check for switch on request
+    if (line.indexOf("SWITCH_ON") >= 0) {
+      on= true;
+      Serial.println("ON");
+    }
+    
+    // check for switch off request
+    else if (line.indexOf("SWITCH_OFF") >= 0) {
+      on= false;
+      valveState = LOW;
+      setPosition(valveState);
+      Serial.println("OFF");
+    }
+    
+    // check for set valve position on request
+    else if (line.indexOf("SET_VALVE_ON") >= 0) {
+      valveState = HIGH;
+      setPosition(valveState);
+      Serial.println(valveState);
+    }
+    
+    // check for set valve position off request
+    else if (line.indexOf("SET_VALVE_OFF") >= 0) {
+      valveState = LOW;
+      setPosition(valveState);
+      Serial.println(valveState);
+    }
+    
+    // check for status request
+    else if (line.indexOf("STATUS") >= 0) {
+      if (on == false) {
+        Serial.println("OFF");
+      }
+      else if (errs.length() > 0) {
+        Serial.println(errs);
+      }
+      else if (hold == true) {
+        Serial.println("HOLD");
+      }
+      else {
+        Serial.println("ON");
+      }
+    }
+    
+    // check for position request
+    else if (line.indexOf("POS") >= 0) {
       Serial.println(valveState);
     }
     
     // if serial input is an integer, store this as the switching delay
     else if (line.toInt() > 0) {
       switchDelay = line.toInt();
-      
       Serial.print("DELAY: ");
       Serial.println(switchDelay);
     }
   }
   
   
-  
-  // check to make sure spectrometer hasn't triggered acquisition to start
-  // if it has then start the holding delay.
-  // Note that Bruker spectrometers use active low logic.
-  if (analogRead(ttl) < 500) {
-    if (hold == false && debug == true){
-      Serial.println("Start hold");
-    }  
-    hold = true;
-    holdStart = millis();
-  } 
-  
-  
-  // get current time in milliseconds
-  unsigned long currentMillis = millis();
-  
-  
-  // if in hold mode wait for the hold delay to end
-  if (hold == true) {
-    if (currentMillis - holdStart >= (holdDelay*1000)) {
-      hold = false;
-      if (debug == true) { 
-        Serial.println("End hold");
+  if (on == true) {
+    // check to make sure spectrometer hasn't triggered acquisition to start
+    // if it has then start the holding delay. If no input was detected on the
+    // TTL line when the module was started then this step is skipped.
+    // Note that Bruker spectrometers use active low logic.
+    if (analogRead(ttl) < 500 && ttl_working == true) { 
+      hold = true;
+      holdStart = millis();
+    } 
+    
+    
+    // get current time in milliseconds
+    unsigned long currentMillis = millis();
+    
+    
+    // if in hold mode wait for the hold delay to end
+    if (hold == true) {
+      if (currentMillis - holdStart >= (holdDelay*1000)) {
+        hold = false;
       }
     }
-  }
-  
-  
-  // otherwise check to see if it's time to switch the valve; that is, if the 
-  // difference between the current time and last time you switched the valve is 
-  // bigger than the switching time delay.
-  else if (currentMillis - previousMillis >= (switchDelay*1000)) {
     
-    previousMillis = currentMillis;
-  
-    // if the valves are off turn them on and vice-versa:
-    if (valveState == LOW) {
-      valveState = HIGH;
-    } else {
-      valveState = LOW;
-    }
+    
+    // otherwise check to see if it's time to switch the valve; that is, if the 
+    // difference between the current time and last time you switched the valve is 
+    // bigger than the switching time delay.
+    else if (currentMillis - previousMillis >= (switchDelay*1000)) {
       
-    // set the relays with the valveState of the variable:
-    digitalWrite(LED_BUILTIN, valveState);
-    digitalWrite(relay3, valveState);
-    digitalWrite(relay4, valveState);
+      previousMillis = currentMillis;
+    
+      // if the valves are off turn them on and vice-versa:
+      if (valveState == LOW) {
+        valveState = HIGH;
+      } else {
+        valveState = LOW;
+      }
+        
+      setPosition(valveState);
+    }
   }
+}
+
+void setPosition(int state) {
+  // set the relays with the valveState of the variable:
+    digitalWrite(LED_BUILTIN, state);
+    digitalWrite(relay3, state);
+    digitalWrite(relay4, state);
 }
